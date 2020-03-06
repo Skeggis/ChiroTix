8ea913c866526107e63095b53b082dda74f96e26
Skjaldbaka17 <Skjaldbaka17@gmail.com>
2020-03-06 14:18:55 +0000

    Added button for downloading bought tickets in receipt/orderDetails

--- client/src/Components/OrderDetails/OrderDetails.js
+++ client/src/Components/OrderDetails/OrderDetails.js
@@ -11,7 +11,8 @@ export default function OrderDetails(props) {
   const {
     orderDetails,
     tickets,
-    chiroInfo
+    chiroInfo,
+    pdfHref
   } = props
 
   const receipt = orderDetails.receipt
@@ -25,6 +26,16 @@ export default function OrderDetails(props) {
 
   return (
     <div className='orderDetails'>
+      <div className="orderDetails__fixedContainer">
+      <div className='orderDetails__downloadButtonContainer'>
+          <Button
+            href={pdfHref}
+            target="_blank"
+          >
+            Tickets
+          </Button>
+        </div>
+      </div>
       <div className='orderDetails__container'>
         <Icon type="check-circle" theme="twoTone" twoToneColor="#52c41a" className='orderDetails__success' />
         <h1 style={{ fontWeight: 400 }}>We've received your order</h1>
@@ -121,13 +132,6 @@ export default function OrderDetails(props) {
 
       <div className='orderDetails__infoContainer'>
         <ShareButtons />
-        <div className='orderDetails__downloadButtonContainer'>
-          <Button
-
-          >
-            Tickets
-          </Button>
-        </div>
       </div>
 
     </div>
--- client/src/Pages/TicketsPage/TicketsPage.js
+++ client/src/Pages/TicketsPage/TicketsPage.js
@@ -435,7 +435,8 @@ function TicketsPage(props) {
                 setPaypalProcessingLoading={setPaypalProcessingLoading}
             />
     } else if (current === 3) {
-        componentToShow = <OrderDetails orderDetails={orderDetails} chiroInfo={chiroInfo} tickets={ticketTypes} />
+        let pdfHref = `${process.env.REACT_APP_SERVER_URL}/api/tickets/downloadMyTickets/${orderDetails.orderId}`
+        componentToShow = <OrderDetails orderDetails={orderDetails} chiroInfo={chiroInfo} tickets={ticketTypes} pdfHref={pdfHref}/>
         ref.current.socket.disconnect(true)
     }
 
